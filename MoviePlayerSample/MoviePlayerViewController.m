ac38d1597b6316ef2fb52bf25ebd0ab4e93702ae
heaven <heaven@mintech.kr>
2013-08-12 10:51:16 +0900

    Code 정리

--- MoviePlayerSample/MoviePlayerViewController.m
+++ MoviePlayerSample/MoviePlayerViewController.m
@@ -34,8 +34,9 @@
     
     self.movieUrlField.text = @"http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8";
     
-    self.moviePlayerController = [MPMoviePlayerController new];
+    [self prepareMoviePlayer];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -43,15 +44,59 @@
 }
 
 #pragma mark - Movie Player Controller
+
+- (void)prepareMoviePlayer
+{
+    self.moviePlayerController = [MPMoviePlayerController new];
+    if (![self.moviePlayerContainerView.subviews containsObject:self.moviePlayerController.view]) {
+        self.moviePlayerController.view.frame = self.moviePlayerContainerView.bounds;
+        [self.moviePlayerContainerView addSubview:self.moviePlayerController.view];
+    }
+}
+
+// Local
+- (NSURL *)movieUrl
+{
+    NSString *moviePath = [[NSBundle mainBundle] pathForResource:@"Movie" ofType:@"m4v"];
+    if (moviePath)
+        return [NSURL fileURLWithPath:moviePath];
+    
+    return nil;
+}
+
+// Streaming
+- (NSURL *)streamingUrl
+{
+    if (self.movieUrlField.text.length > 0)
+		return [NSURL URLWithString:self.movieUrlField.text];
+    
+    return nil;
+}
+
+-(void)playMovieStream:(NSURL *)movieFileURL
+{
+    MPMovieSourceType movieSourceType = MPMovieSourceTypeUnknown;
+    
+    if ([movieFileURL.pathExtension compare:@"m3u8" options:NSCaseInsensitiveSearch] == NSOrderedSame)
+        movieSourceType = MPMovieSourceTypeStreaming;
+    else if ([movieFileURL.pathExtension compare:@"m4v" options:NSCaseInsensitiveSearch] == NSOrderedSame)
+        movieSourceType = MPMovieSourceTypeFile;
+    
+    [self createAndPlayMovieForURL:movieFileURL sourceType:movieSourceType];
+}
+
 -(void)createAndPlayMovieForURL:(NSURL *)movieURL sourceType:(MPMovieSourceType)sourceType
 {
-    [self createAndConfigurePlayerWithURL:movieURL sourceType:sourceType];
+    [self configurePlayerWithURL:movieURL sourceType:sourceType];
+    
+    /* Apply the user movie preference settings to the movie player object. */
+    [self applyUserSettingsToMoviePlayer];
     
     /* Play the movie! */
     [self.moviePlayerController play];
 }
 
--(void)createAndConfigurePlayerWithURL:(NSURL *)movieURL sourceType:(MPMovieSourceType)sourceType
+- (void)configurePlayerWithURL:(NSURL *)movieURL sourceType:(MPMovieSourceType)sourceType
 {
     /* Specify the URL that points to the movie file. */
     self.moviePlayerController.contentURL = movieURL;
@@ -59,9 +104,6 @@
     /* If you specify the movie type before playing the movie it can result
      in faster load times. */
     self.moviePlayerController.movieSourceType = sourceType;
-    
-    /* Apply the user movie preference settings to the movie player object. */
-    [self applyUserSettingsToMoviePlayer];
 }
 
 -(void)applyUserSettingsToMoviePlayer
@@ -72,46 +114,19 @@
     
     self.moviePlayerController.backgroundView.backgroundColor = [UIColor lightGrayColor];
     
-    if (![self.moviePlayerContainerView.subviews containsObject:self.moviePlayerController.view]) {
-        self.moviePlayerController.view.frame = self.moviePlayerContainerView.bounds;
-        [self.moviePlayerContainerView addSubview:self.moviePlayerController.view];
-    }
-    
     /* Indicate the movie player allows AirPlay movie playback. */
     self.moviePlayerController.allowsAirPlay = YES;
     
     [self.moviePlayerController prepareToPlay];
 }
 
--(void)playMovieStream:(NSURL *)movieFileURL
-{
-    MPMovieSourceType movieSourceType = MPMovieSourceTypeUnknown;
-    
-    if ([movieFileURL.pathExtension compare:@"m3u8" options:NSCaseInsensitiveSearch] == NSOrderedSame)
-        movieSourceType = MPMovieSourceTypeStreaming;
-    
-    [self createAndPlayMovieForURL:movieFileURL sourceType:movieSourceType];
-}
-
-- (NSURL *)movieUrl
-{
-    NSString *moviePath = [[NSBundle mainBundle] pathForResource:@"Movie" ofType:@"m4v"];
-    if (moviePath)
-        return [NSURL fileURLWithPath:moviePath];
-    
-    return nil;
-}
-
 - (IBAction)playButtonTouched:(UIButton *)sender {
-	if (self.movieUrlField.text.length > 0)
-	{
-		NSURL *theMovieURL = [NSURL URLWithString:self.movieUrlField.text];
-		if (theMovieURL)
-		{
-			if ([theMovieURL scheme])	// sanity check on the URL
-                [self playMovieStream:theMovieURL];
-		}
-	}    
+    NSURL *theMovieURL = [self streamingUrl];   //  [self movieUrl];
+    if (theMovieURL)
+    {
+        if ([theMovieURL scheme])	// sanity check on the URL
+            [self playMovieStream:theMovieURL];
+    }
 }
 
 @end
